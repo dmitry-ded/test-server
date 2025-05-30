ef5776e2a894a1061bf8b3825fe94d123977df91
sosok11let <118977309+sosok11let@users.noreply.github.com>
2025-05-30 16:50:17 +0300

    Исправил структуру для Vercel

--- api/index.js
+++ api/index.js
@@ -1,37 +1,31 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 
 const app = express();
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const usersDB = [
-  {name: "Anna"}, 
-  {name: "Ben"}, 
-  {name: "Clara"},
-  {name: "David"}, 
-  {name: "Emma"}, 
-  {name: "Finn"}, 
-  {name: "Grace"}, 
-  {name: "Henry"}, 
-  {name: "Isla"}, 
-  {name: "Jack"},
-]
+  { name: "Anna" },
+  { name: "Ben" },
+  { name: "Clara" },
+  { name: "David" },
+  { name: "Emma" },
+  { name: "Finn" },
+  { name: "Grace" },
+  { name: "Henry" },
+  { name: "Isla" },
+  { name: "Jack" },
+];
+
+app.get("/api/short-polling", (req, res) => {
+  const { last } = req.query;
+  const fetchUsers = usersDB.slice(last, usersDB.length);
+  res.status(200).json({ users: fetchUsers, last: usersDB.length });
+});
 
 app.get("/", (req, res) => {
-  res.send("Hello, server")
-})
-
-app.listen(4000, () => {
-  console.log(`Server is running port=${4000}"`);
-})
-
-app.get("/short-polling", (req, res) => {
-  const { last } = req.query
-  const fetchUsers = usersDB.slice(last, usersDB.length);
-
-  res.status(200).json({ users: fetchUsers, last: usersDB.length })
-})
+  res.send("Hello, server");
+});
 
 export default app;
-
