a411c1e541515cd108137a911870fecad90ddf4c
sosok11let <118977309+sosok11let@users.noreply.github.com>
2025-05-30 17:47:31 +0300

    1

--- index.js
+++ index.js
@@ -28,4 +28,9 @@ app.get("/", (req, res) => {
   res.send("Hello, server");
 });
 
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 export default app;
